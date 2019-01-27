02336129293b67f86388a70d1aab3c5e36d97f1a
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2019-01-26 20:30:27 -0800

    some styling fixes

--- client/src/components/Navbar-profile/index.js
+++ client/src/components/Navbar-profile/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./style.css";
-import SearchBar from "../SearchBar";
 import { Navbar, NavItem } from "react-materialize";
 import SideNavBar from "../SideNav";
 import Login from "../Login";
+import SignUp from "../SignUp";
 
 
 // By extending the React.Component class, Counter inherits functionality from it
@@ -16,7 +16,14 @@ function NavbarProfile(props) {
             {/* //use bootstrap to create a navbar */}
             <div className="prof-nav-margin">
                 <Navbar className="top-nav brand-logo cyan darken-4" brand={<Link to="/" className="brand-logo">GardenBuddy</Link>} right>
-
+                    <NavItem>Flowers</NavItem>
+                    <NavItem>Vegetables</NavItem>
+                    <NavItem>Herbs</NavItem>
+                    <NavItem>Fruits</NavItem>
+                    <NavItem className="blue-grey darken-3" style={ {fontWeight: "bold"} }><Link to="/teamprofile">Our Team</Link></NavItem>
+                    <NavItem className="amber"><Login /></NavItem>
+                    <NavItem><SignUp /></NavItem>
+                    <NavItem><SideNavBar /></NavItem>
                 </Navbar>
             </div>
         </wrapper>
