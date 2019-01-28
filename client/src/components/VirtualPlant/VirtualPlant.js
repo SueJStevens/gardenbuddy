d43fd19bfee14e0e29c5d22aaa61649205366634
Narasimhan Ramesh <narasimhan.ramesh5@gmail.com>
2019-01-27 22:02:32 -0800

    Updated virtual garden functionality

--- client/src/components/VirtualPlant/VirtualPlant.js
+++ client/src/components/VirtualPlant/VirtualPlant.js
@@ -1,13 +1,50 @@
 import React from "react";
-import{Card, CardTitle} from "react-materialize";
-
+import{Card, CardTitle, Button} from "react-materialize";
 import "./VirtualPlant.css";
+
+const moment = require('moment');
+
+let warningStyle = {
+  color: 'darkorange',
+  fontWeight:'bold'
+}
+
+const PlantInfo = (plantName, lastWatered, daysOverdue, wateringFrequency, handleWateringDone) => {
+
+  if(daysOverdue > 0) {
+
+    let dayOrDays = (daysOverdue > 1 ? "days" : "day");
+
+    return(
+      <div>
+        <p style={warningStyle}>Last watered : {moment(lastWatered).format("ddd MMM Do")}</p>
+        <p style={warningStyle}>Watering overdue by {daysOverdue} {dayOrDays}</p>
+        <Button onClick={ () => handleWateringDone(plantName) }>
+          <i class="fas fa-tint"></i>done watering
+        </Button>
+      </div>
+    )
+  } else {
+    let nextWatering = moment(lastWatered).add(wateringFrequency, 'days');
+    //console.log(nextWatering);
+    return(
+      <div>
+         <p>Last watered : {moment(lastWatered).format("ddd MMM Do")} </p>
+         <p>Next watering due on : {nextWatering.format("ddd MMM Do")}</p>
+      </div>
+    )
+  }
+}
 
 /**
  * This is the VirtualPlant component. It represents
  * a virtual plant in the user's virtual garden.
  * 
  * It is essentially a Materialize Card.
+ * 
+ * We use the 'reveal' attribute to reveal additional info
+ * on the plant - e.g. when the next watering is due, and
+ * if watering is overdue.
  * 
  * @param {*} props 
  *            
@@ -18,16 +55,38 @@ import "./VirtualPlant.css";
  *                     its time to water this plant
  */
 
-const VirtualPlant = props => (
-  <Card className='small'
-    header={<CardTitle image={props.image}></CardTitle>}>
-    
-    {/* The plant name */}
-    <b>{props.name}</b>
+const VirtualPlant = props => {
 
-    <p>Last Watered : {props.lastWatered}</p>
+  let nextWatering = moment(props.lastWatered).add(props.wateringFrequency, 'days');
 
-  </Card>
-);
+  let { plantImage, plantName, lastWatered, daysOverdue, wateringFrequency, handleWateringDone } = props;
+
+  return(
+    <Card className='small'
+      header={<CardTitle reveal image={plantImage} waves='light'/>}
+
+      reveal={PlantInfo(plantName, lastWatered, daysOverdue, wateringFrequency, handleWateringDone)}
+
+      title={daysOverdue > 0 ? 
+               <span style={warningStyle}>{plantName}</span>
+              :<b>{plantName}</b>}
+    >
+      {/* If watering is overdue, display a warning message under card image.
+          Otherwise display the next watering date */}
+      {daysOverdue > 0 ? 
+        <p style={warningStyle}>
+          {/*<i class="fas fa-exclamation-triangle" style={warningStyle}></i>*/}
+          Watering overdue
+        </p> 
+        : 
+        <p>Next watering due on:
+          <br></br> 
+          <b>{nextWatering.format("ddd MMM Do")}</b>
+        </p>
+      }
+
+   </Card>
+  )
+}
 
 export default VirtualPlant;
