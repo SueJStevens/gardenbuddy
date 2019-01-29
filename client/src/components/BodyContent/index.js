5b22e0480b39599c952bdd01d56d55c9a282ad1e
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2019-01-28 22:54:15 -0800

    deleted excess stuff

--- client/src/components/BodyContent/index.js
+++ client/src/components/BodyContent/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "./style.css";
-import FlowerIcon from "../FlowerIcon";
-import LeafIcon from "../LeafIcon";
-import FruitIcon from "../FruitIcon";
-import { Carousel } from "react-materialize";
 import Intro from "../Intro";
-import SearchBar from "../SearchBar";
 import NewComerIns from "../NewComerIns";
 
 function BodyContent(props) {
