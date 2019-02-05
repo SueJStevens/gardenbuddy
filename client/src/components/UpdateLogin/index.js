0397c435aca966f514af868551942b25a9edbd83
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2019-02-04 21:44:28 -0800

    fixed more bugs

--- client/src/components/UpdateLogin/index.js
+++ client/src/components/UpdateLogin/index.js
@@ -14,15 +14,15 @@ function UpdateLogin(props) {
                 <Table className="">
                     <tbody>
                         <tr>
-                            <td className="user-info"><i className="fas fa-user"></i>Username:</td>
+                            <td className="thead"><i className="fas fa-user"></i>Username:</td>
                             <td><input value="Test User" /></td>
                         </tr>
                         <tr>
-                            <td className="user-info"><i className="fas fa-envelope"></i>Email:</td>
+                            <td className="thead"><i className="fas fa-envelope"></i>Email:</td>
                             <td><input value="test@user.com" /></td>
                         </tr>
                         <tr>
-                            <td className="user-info"><i className="fas fa-key"></i>Password:</td>
+                            <td className="thead"><i className="fas fa-key"></i>Password:</td>
                             <td><input placeholder="******" /></td>
                         </tr>
                     </tbody>
