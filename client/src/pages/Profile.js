cc66d59de556740c801103f7be39e82898a63fc1
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2019-02-07 15:11:35 -0800

    attempting to link profile stats

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,12 +3,33 @@ import NavbarProfile from "../components/Navbar-profile";
 import ProfileHeader from "../components/ProfileHeader";
 import ProfileBody from "../components/ProfileBody";
 import Wrapper from "../components/Wrapper";
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
+
 
 
 // By extending the React.Component class, Counter inherits functionality from it
 class Profile extends Component {
 
+    state = {
+        loggedIn: false
+    }
+
+    componentDidMount() {
+        axios.get("/auth/user").then((response) => {
+          console.log(response.data);
+    
+          if (response.data) {
+          this.setState({
+            loggedIn: true
+          })
+        }
+        })
+      }
+
 render () {
+
+    if (this.state.loggedIn) {
 
     // The render method returns the JSX that should be rendered
     return (
@@ -19,6 +40,11 @@ render () {
             <ProfileBody user={this.props.user}/>
         </Wrapper>
     );
+} else {
+    return (
+        <Redirect to={{pathname:"/", state:this.props.user}}></Redirect>
+    )
+}
 }
 };
 
