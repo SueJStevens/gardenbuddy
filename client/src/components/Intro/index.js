b9d0f1e884a85042a9caa95ba7517a83a0747095
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2019-02-07 20:42:19 -0800

    changed intro

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -6,7 +6,8 @@ function Intro(props) {
         <div className="introParagraph">
             <h3 id="intro-title">Who Are We?</h3>
             <p id="intro-text">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                You can't put a plant in the ground and expect it to grow.  It needs water and love and attention. There is great satisfacton when seeds sprout, plants grow, flowers bloom and dinner is havested from the back yard.  GardenBuddy offers our users advise, friendship and encouragement to grow myriad of flowers, vegetables, herbs and fruits regardless of whether they live in the heartland or the desert.  GardenBuddy offers advise and information so you know what will grow in your hardiness zone and the best time to plant.  Plus, we'll send you water reminders so your plants are never thirsty.
+                Happy Harvest!
             </p>
         </div>
     )
