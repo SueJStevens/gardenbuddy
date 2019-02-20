6ea6b13b71f2d15d350f95fa24529536179827c0
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2019-02-20 10:50:57 -0800

    fixed typos

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -6,7 +6,7 @@ function Intro(props) {
         <div className="introParagraph">
             <h3 id="intro-title">Who Are We?</h3>
             <p id="intro-text">
-                You can't put a plant in the ground and expect it to grow.  It needs water and love and attention. There is great satisfacton when seeds sprout, plants grow, flowers bloom and dinner is havested from the back yard.  GardenBuddy offers our users advice, friendship and encouragement to grow myriad of flowers, vegetables, herbs and fruits regardless of whether they live in the heartland or the desert.  GardenBuddy offers advise and information so you know what will grow in your hardiness zone and the best time to plant.  Plus, we'll send you water reminders so your plants are never thirsty.
+                You can't put a plant in the ground and expect it to grow.  It needs water and love and attention. There is great satisfaction when seeds sprout, plants grow, flowers bloom and dinner is harvested from the back yard.  GardenBuddy offers our users advice, friendship and encouragement to grow myriad of flowers, vegetables, herbs and fruits regardless of whether they live in the heartland or the desert.  GardenBuddy offers advise and information so you know what will grow in your hardiness zone and the best time to plant.  Plus, we'll send you water reminders so your plants are never thirsty.
                 Happy Harvest!
             </p>
         </div>
